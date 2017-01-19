a67e7741e73864a38ae903eeba6e55c54b18d74f
apatterson1 <apatterson@hs-gc.org>
2017-01-19 23:15:57 +0000

    first commit

---
+++ mta.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    
+$.getJSON("https://ajar-target.gomix.me/stops", function(response){ 
+    
+    function MtaThing() {
+        document.getElementById("#MtaStuff");
+        
+    }
+    console.log(response);
+});
+});
